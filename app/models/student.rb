9f06ac81af9d9b7ce72a814982cdd7bd25f9336b
Pedro Brost <pedro.brost97@gmail.com>
2017-12-20 19:22:07 -0300

    Add results to all exams

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,7 +7,15 @@ class Student < ApplicationRecord
   end
 
   def score_for(exam)
-    results.where(exam_id: exam.id).take.try(:score) || "Absent"
+    results.where(exam_id: exam.id).take.try(:score)
+  end
+
+  def attended_to?(exam)
+    self.score_for(exam).present?
+  end
+
+  def passed?(exam)
+    self.score_for(exam) >= exam.minimum
   end
 
 end
