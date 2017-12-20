ad0334f2b237bc1f3d9e46abe77160b4d48314dd
Pedro Brost <pedro.brost97@gmail.com>
2017-12-20 19:21:41 -0300

    Remove blank validation on results edit

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -2,6 +2,6 @@ class Exam < ApplicationRecord
   has_many :results
   belongs_to :course
 
-  accepts_nested_attributes_for :results,  reject_if: proc { |attributes| attributes['score'].blank? }
+  accepts_nested_attributes_for :results
 
 end
