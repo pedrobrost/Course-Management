d579e7e7d987989cc1bce179c6c288c585519e3c
Pedro Brost <pedro.brost97@gmail.com>
2017-12-21 19:25:59 -0300

    Add exam tests

--- test/models/exam_test.rb
+++ test/models/exam_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class ExamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should assert zero approved" do
+    assert_equal(0, exams(:one).approved)
+  end
+
+  test "should assert two approved" do
+    assert_equal(2, exams(:two).approved)
+  end
+
+  test "should assert three failed" do
+    assert_equal(3, exams(:one).failed)
+  end
+
+  test "should assert two failed" do
+    assert_equal(2, exams(:two).failed)
+  end
+
+  test "should assert zero absent students" do
+    assert_equal(0, exams(:two).absent_students)
+  end
+
+  test "should assert one absent students" do
+    assert_equal(1, exams(:one).absent_students)
+  end
+
+  test "should assert zero approved percentage" do
+    assert_equal(0, exams(:one).approved_percentage)
+  end
+
+  test "should assert 50 approved percentage" do
+    assert_equal(50, exams(:two).approved_percentage)
+  end
+
 end
