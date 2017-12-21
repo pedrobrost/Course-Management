38f312c9f3fe048d4a7730b7d230272cd489a85e
Pedro Brost <pedro.brost97@gmail.com>
2017-12-21 19:26:07 -0300

    Add result tests

--- test/models/result_test.rb
+++ test/models/result_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ResultTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not assert approved" do
+    assert_not results(:one).approved?
+  end
+
+  test "should assert approved" do
+    assert results(:five).approved?
+  end
+
 end
