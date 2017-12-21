0ff7293491e6ec4a095a0761136c6bcd510bbeee
Pedro Brost <pedro.brost97@gmail.com>
2017-12-21 19:25:53 -0300

    Add course tests

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -4,4 +4,35 @@ class CourseTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    @course = Course.new
+  end
+
+  test 'should not save course without title' do
+    assert_not @course.save
+  end
+
+  test 'should not save course without year' do
+    assert_not @course.save
+  end
+
+  test "year should not be too old" do
+    @course.year = 1899
+    assert_not @course.save
+  end
+
+  test "year should not be a string" do
+    @course.year = "dos mil cuatro"
+    assert_not @course.save
+  end
+
+  test "should not have students" do
+    assert_not courses(:two).has_students?
+  end
+
+  test "should have students" do
+    assert courses(:one).has_students?
+  end
+  
 end
