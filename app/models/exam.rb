0684847d0568d16dcd550a030b33d51ae3ce9806
Pedro Brost <pedro.brost97@gmail.com>
2017-12-20 21:03:21 -0300

    Add summary in course results

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -2,6 +2,22 @@ class Exam < ApplicationRecord
   has_many :results
   belongs_to :course
 
-  accepts_nested_attributes_for :results
+  accepts_nested_attributes_for :results, reject_if: proc { |a| a['score'].blank? }
+
+  def approved
+    results.select(&:approved?).size
+  end
+
+  def failed
+    results.size - self.approved
+  end
+
+  def absent_students
+    course.students.size - results.size
+  end
+
+  def approved_percentage
+    (approved * 100) / course.students.size
+  end
 
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,4 +1,9 @@
 class Result < ApplicationRecord
   belongs_to :exam
   belongs_to :student
+
+  def approved?
+    score.try(:>=, exam.minimum)
+  end
+
 end
