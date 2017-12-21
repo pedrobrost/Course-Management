01904c626a044b770e5033d31021f8590e9c1551
Pedro Brost <pedro.brost97@gmail.com>
2017-12-21 19:26:18 -0300

    Add student tests

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should assert 8.9 score for exam" do
+    assert_equal(8.9, students(:one).score_for(exams(:one)))
+  end
+
+  test "should assert attended to exam" do
+    assert students(:one).attended_to?(exams(:one))
+  end
+
+  test "should not assert attended to exam" do
+    assert_not students(:four).attended_to?(exams(:one))
+  end
+
+  test "should assert exam approved" do
+    assert students(:one).passed?(exams(:two))
+  end
+
+  test "should not assert exam approved" do
+    assert_not students(:one).passed?(exams(:one))
+  end
+
 end
