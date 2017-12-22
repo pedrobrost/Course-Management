5510d9fc79739c1ff1309be00385b9630910f046
Pedro Brost <pedro.brost97@gmail.com>
2017-12-22 18:01:31 -0300

    Add course tests

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -18,9 +18,31 @@ class CourseTest < ActiveSupport::TestCase
     assert_not @course.save
   end
 
+  test "year should not be too distant" do
+    @course.year = 3000
+    assert_not @course.save
+  end
+
   test "year should not be a string" do
     @course.year = "dos mil cuatro"
     assert_not @course.save
+  end
+
+  test 'should not destroy if it has students or exams' do
+    assert_not courses(:one).destroy
+  end
+
+  test 'should destroy if it is empty' do
+    assert courses(:two).destroy
+  end
+
+  test 'year should be unique' do
+    course1 = Course.new
+    course2 = Course.new
+    course1.year = 2020
+    course2.year = 2020
+    course1.save
+    assert_not course2.save
   end
 
   test "should not have students" do
