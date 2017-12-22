bf31428f0250b2ec46f056b5b493c20eead39b04
Pedro Brost <pedro.brost97@gmail.com>
2017-12-22 17:02:45 -0300

    Add exam year validation

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -5,6 +5,7 @@ class Exam < ApplicationRecord
   accepts_nested_attributes_for :results, reject_if: proc { |a| a['score'].blank? }
 
   validates_presence_of :title, :date, :minimum
+  validate :correct_year
   validates :minimum,
     numericality: true
 
@@ -22,6 +23,11 @@ class Exam < ApplicationRecord
 
   def approved_percentage
     (approved * 100.00) / course.students.size
+  end
+
+  private
+  def correct_year
+    errors.add(:date, "es anterior a el año del curso") unless date.year >= course.year
   end
 
 end
