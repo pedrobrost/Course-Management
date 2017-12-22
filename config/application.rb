1b323d8d617870e89c6c8e961ea291652a7ad15b
Pedro Brost <pedro.brost97@gmail.com>
2017-12-21 22:33:05 -0300

    Add i18n for validation messages and devise

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module CourseManagement
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.i18n.default_locale = :es
     config.assets.paths << Rails.root.join("vendor")
 
     # Settings in config/environments/* take precedence over those specified here.
